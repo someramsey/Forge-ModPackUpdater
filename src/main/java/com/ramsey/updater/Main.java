3fae0ff68932db2ed2aef99428ea7691796b3174
Ramsey <someramseyy@gmail.com>
2024-06-29 21:04:59 +0200

    trying to open the screen

--- src/main/java/com/ramsey/updater/Main.java
+++ src/main/java/com/ramsey/updater/Main.java
@@ -1,9 +1,17 @@
 package com.ramsey.updater;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 
@@ -15,5 +23,23 @@ public class Main {
     public Main() {
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(40000);
+                Minecraft.getInstance().setScreen(new UpdateScreen());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
---
+++ src/main/java/com/ramsey/updater/UpdateScreen.java
@@ -0,0 +1,11 @@
+package com.ramsey.updater;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class UpdateScreen extends Screen {
+    protected UpdateScreen() {
+        super(Component.literal("dw"));
+    }
+}
+
