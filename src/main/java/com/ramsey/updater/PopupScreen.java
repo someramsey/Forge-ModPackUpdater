00ef0e74f89a1d74f3f0258052fe025535829535
Ramsey <someramseyy@gmail.com>
2024-06-30 23:01:21 +0200

    Setting up the sate machine

--- src/main/java/com/ramsey/updater/PopupScreen.java
+++ src/main/java/com/ramsey/updater/PopupScreen.java
@@ -6,47 +6,67 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.MultiLineLabel;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 
 public class PopupScreen extends Screen {
     private static final Component updateAvailableMessage = Component.translatable("gui.updater.update_available");
 
-    private MultiLineLabel message;
-    private Button button;
+    private State state = State.Confirm;
+    private MultiLineLabel statusLabel;
+    private Button continueButton;
 
     public PopupScreen() {
-        super(Component.literal("dw"));
+        super(Component.translatable("gui.updater.title"));
     }
 
     @Override
     protected void init() {
-        this.message = MultiLineLabel.create(this.font, updateAvailableMessage, this.width - 50);
+        this.statusLabel = MultiLineLabel.create(this.font, updateAvailableMessage, this.width - 50);
+    }
+
+    private void renderDownloadButton() {
+        this.continueButton = this.addRenderableWidget(new Button(this.width / 2 - 100, this.height - 25, 200, 20, Component.translatable("gui.updater.download"), (button) -> {
+
+        }));
     }
 
     @Override
     public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
         this.renderDirtBackground(0);
 
-        this.message.renderCentered(pPoseStack, this.width / 2, 90);
+        this.statusLabel.renderCentered(pPoseStack, this.width / 2, 90);
 
-//
-//        int width = 150;
-//
-//        this.addRenderableWidget(new Button((this.width - width) / 2, 120, width, 20, Component.literal("text"), (button) -> {
-//
-//        }));
-        renderLoadingBar(pPoseStack, (this.width - 150) / 2, 120, 150, 10, 0.5f);
+        switch (this.state) {
+            case Confirm:
+                this.renderDownloadButton();
+                break;
+                case Downloading:
+                renderLoadingBar(pPoseStack,0.5f);
+        }
+
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
     }
 
-    private void renderLoadingBar(PoseStack pPoseStack, int pX, int pY, int pWidth, int pHeight, float pProgress) {
+    private void renderLoadingBar(PoseStack pPoseStack, float pProgress) {
+        int x = (this.width - 150) / 2;
+        int y = 120;
+
+        int width = 150;
+        int height = 10;
+
         int margin = 1;
         int innerMargin = 2;
 
         int fillColor = -1;
         int backgroundColor = -16777216;
 
-        GuiComponent.fill(pPoseStack, pX, pY, pX + pWidth, pY + pHeight, fillColor);
-        GuiComponent.fill(pPoseStack, pX + margin, pY + margin, pX + pWidth - margin, pY + pHeight - margin, backgroundColor);
-        GuiComponent.fill(pPoseStack, pX + innerMargin, pY + innerMargin, (int) (pX + (pWidth * pProgress) - innerMargin), pY + pHeight - innerMargin, fillColor);
+        GuiComponent.fill(pPoseStack, x, y, x + width, y + height, fillColor);
+        GuiComponent.fill(pPoseStack, x + margin, y + margin, x + width - margin, y + height - margin, backgroundColor);
+        GuiComponent.fill(pPoseStack, x + innerMargin, y + innerMargin, (int) (x + (width * pProgress) - innerMargin), y + height - innerMargin, fillColor);
+    }
+
+    private enum State {
+        Confirm,
+        Downloading
     }
 }
