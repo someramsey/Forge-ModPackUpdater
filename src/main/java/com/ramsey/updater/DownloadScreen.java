30efe3e76b276de89742240a6a19683b7864ec1b
Ramsey <someramseyy@gmail.com>
2024-07-02 14:33:34 +0200

    Added Error Panel

--- src/main/java/com/ramsey/updater/DownloadScreen.java
+++ src/main/java/com/ramsey/updater/DownloadScreen.java
@@ -17,6 +17,7 @@ import net.minecraftforge.client.gui.ScreenUtils;
 import net.minecraftforge.client.gui.widget.ScrollPanel;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.util.Size2i;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -51,15 +52,13 @@ public class DownloadScreen extends Screen {
         this.renderBackground(pPoseStack);
 
         drawCenteredString(pPoseStack, this.font, getMessage(), this.width / 2, 70, textColor);
-        progressBar.render(pPoseStack, pMouseX, pMouseY, pPartialTick, 100, 120, this.width - 200, 10);
-
-
+        progressBar.render(pPoseStack);
 
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
     }
 
     private Component getMessage() {
-        if(progress == null) {
+        if (progress == null) {
             return this.preparingMessage;
         }
 
@@ -67,26 +66,45 @@ public class DownloadScreen extends Screen {
     }
 
 
-    public static class InfoPanel extends ScrollPanel {
+    public class InfoPanel extends ScrollPanel {
+        private final List<String> lines = new ArrayList<>();
+
+        public InfoPanel(int width, int height, int top, int left) {
+            super(minecraft, width, height, top, left);
+        }
+
+        public void setContent(String content) {
+            lines.clear();
+
+            if (content != null) {
+                Collections.addAll(lines, content.split("\n"));
+            }
+        }
 
         @Override
         protected int getContentHeight() {
-            return 0;
+            return font.lineHeight * lines.size();
         }
 
         @Override
         protected void drawPanel(PoseStack poseStack, int entryRight, int relativeY, Tesselator tess, int mouseX, int mouseY) {
+            int y = top + 4 - getScrollAmount();
 
+            for (String line : lines) {
+                if (y > top && y < bottom) {
+                    drawCenteredString(poseStack, font, line, width / 2, y, textColor);
+                }
+
+                y += font.lineHeight;
+            }
         }
 
         @Override
-        public NarrationPriority narrationPriority() {
-            return null;
+        public @NotNull NarrationPriority narrationPriority() {
+            return NarrationPriority.NONE;
         }
 
         @Override
-        public void updateNarration(NarrationElementOutput pNarrationElementOutput) {
-
-        }
+        public void updateNarration(@NotNull NarrationElementOutput pNarrationElementOutput) { }
     }
 }
--- src/main/java/com/ramsey/updater/ProgressBar.java
+++ src/main/java/com/ramsey/updater/ProgressBar.java
@@ -2,16 +2,15 @@ package com.ramsey.updater;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.GuiComponent;
-import net.minecraft.client.gui.components.Widget;
 import org.jetbrains.annotations.NotNull;
 
-public class ProgressBar extends GuiComponent implements Widget {
+public class ProgressBar extends GuiComponent {
     public float progress;
 
-    private int x;
-    private int y;
-    private int width;
-    private int height;
+    private final int x;
+    private final int y;
+    private final int width;
+    private final int height;
 
     private static final int outlineThickness = 1;
     private static final int innerMargin = 2;
@@ -26,17 +25,7 @@ public class ProgressBar extends GuiComponent implements Widget {
         this.height = height;
     }
 
-    public void render(@NotNull PoseStack pPoseStack, int pMouseX, int pMouse, float pPartialTick, int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-
-        render(pPoseStack, pMouseX, pMouse, pPartialTick);
-    }
-
-    @Override
-    public void render(@NotNull PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
+    public void render(@NotNull PoseStack pPoseStack) {
         GuiComponent.fill(pPoseStack, x, y, x + width, y + outlineThickness, outlineColor);
         GuiComponent.fill(pPoseStack, x, y + height - outlineThickness, x + width, y + height, outlineColor);
         GuiComponent.fill(pPoseStack, x, y + outlineThickness, x + outlineThickness, y + height - outlineThickness, outlineColor);
